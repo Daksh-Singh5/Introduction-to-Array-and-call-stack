f4341afde0517e42c55ae20fb39b3bdc1d00144a
Daksh-Singh5 <dakshsingh6612@gmail.com>
2024-12-15 14:56:08 +0530

    hello

---
+++ project.js
@@ -0,0 +1,4 @@
+var person = ["tinu","orange",6,7,"blue","riya"]
+document.write("My name is ",person[0],"<br>")
+document.write("My favroit colour is ",person[1],"<br>")
+document.write("I am in class ",person[3],"<br>")
